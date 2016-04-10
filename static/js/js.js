4f2abfcb3dba54dc9aec44e92250182d803677c7
hsp86 <husipeng86@live.com>
2016-04-10 22:08:51 +0800

    基本功能实现，当前只支持utf-8(中文)文件，文件路径中特殊字符导致出错，需要修改，界面和搜索细节需要修改

--- static/js/js.js
+++ static/js/js.js
@@ -1,22 +1,75 @@
 $(function(){
-    alert("haha");
-
-    function refresh_sl()
+    // 将网页中指定元素中的指定字符串标添加span来显示指定class的颜色
+    function set_color($obj,str)
     {
-        var $form = $(".login-block form");
-        var method = $form[0].method;
-        var action = $form[0].action;
+        obj_str = $obj.text();
+        $obj.html(obj_str.replace(new RegExp(str,'g'),'<span class="set_color">' + str + '</span>'));
+
+    }
+    // 搜索结果点击事件，每次加载后需要重新绑定
+    function re_bind()
+    {
+        $('.result_item').bind('click',function(event) {
+            open_file($(this).children('.item_fname').text());
+        });
+    }
+    // 将网页中选中checkbox内容转化为ajax格式字符串返回
+    function get_search_dir(class_text)
+    {
+        var re_var = "";
+        $('.' + class_text).each(function(index, el) {
+            if(this.checked)
+            {
+                re_var = re_var + "&dir=" + this.value;
+            }
+        });
+        return re_var;
+    }
+    // 搜索内容，通过ajax发送搜索文本和目录
+    function search(search_text)
+    {
+        var method = "get";
+        var action = '/search';
+        var search_data = "search_text=" + search_text + get_search_dir('check_dir');
+        $('.search_result').html("正在搜索...");
+        // alert(search_data);
         $.ajax({
             url: action,
             type: method,
-            data: $form.serialize(),
+            data: search_data,
             success: function(msg){
-                if(msg == 1)
-                {
-                    alert("ok");
-                }
+                $('.search_result').html(msg);
+                re_bind();
+                set_color($('.item_fcontent'),search_text);
             }
         });
     }
-    
+    // 打开文件，通过ajax发送文件名获取文件内容
+    function open_file(file)
+    {
+        var method = "get";
+        var action = '/open_file';
+        var file_name = "file_name=" + file;
+        $.ajax({
+            url: action,
+            type: method,
+            data: file_name,
+            success: function(text){
+                $('.file_name').text(file);
+                $('.file_content').html(text);
+                $('.showfile').css({display: 'block'});
+                set_color($('.file_content'),search_text);
+            }
+        });
+    }
+    // 搜索按钮点击事件
+    search_text = ""
+    $('#search_button').bind('click', function(event) {
+        search_text = $('#search_text').val();
+        search(search_text);
+    });
+    // 显示文件内容中关闭按钮点击事件
+    $('.close').bind('click',function(event) {
+        $('.showfile').css({display: 'none'});
+    });
 })
--- webpy_main.py
+++ webpy_main.py
@@ -1,6 +1,8 @@
 #-*- coding: utf-8 -*-
-import web,sys
+import web
+import sys,os
 import sqlite3
+import re
 
 import config
 dbfile = config.dbfile
@@ -11,15 +13,15 @@ temp_dir = config.temp_dir
 
 urls = (
     '/','index',
-    '/refresh_sl','refresh_sl',
-    '/add/(.*)','add'#有（.*）则可以传递参数add中的na参数，而且URL中（/add/smt）的smt前的/不能省略，没有smt则na为空.
-                     #即可以用正则表达式，如/(test1|test2)则匹配/test1和/test2，而且这里的test1或test2也会作为参数传入，如上na
+    '/search','search',
+    '/open_file','open_file'
     )
 
 render = web.template.render(temp_dir)
 
 config.search_list = [] # 保存所有要搜索的绝对路径，将search_list放到config模块，直接用全局变量则不对
 
+# 重新加载目录，更新search_list
 def reload_db():
     config.search_list = [] # 刷新时删除之前目录
     conn = sqlite3.connect(dbfile)
@@ -46,22 +48,81 @@ class index:
             load_dir.append(item)
         return render.index(load_dir)
 
-class refresh_sl:
-    def GET(self):
-        reload_db()
-        r_str = "<div>"
-        for item in config.search_list:
-            r_str = r_str + item
-        r_str = r_str + "</div>"
-        return r_str
+# 从指定目录（path）中所有文件中搜索内容（search_text）
+def search_file(search_text,path):
+    res = []
+    files = os.listdir(path)
+    for f in files:
+        path_file = path + '/' + f
+        if(os.path.isfile(path_file)):
+            try:
+                fid = open(path_file)
+                file_str = fid.read().decode('utf-8') # 搜索前将读取的内容以utf-8解码
+            except:
+                print u"当前只支持utf-8的文件！"
+                continue
+            finally:
+                fid.close()
+            pattern = re.compile(search_text)
+            s = pattern.search(file_str)
+            if s != None:
+                res_item = {}
+                start_index = 0; # 判断是否超出文件首末
+                end_index = len(file_str);
+                if start_index < s.span()[0] - 10:
+                    start_index = s.span()[0] - 10
+                if end_index > s.span()[1] + 20:
+                    end_index = s.span()[1]
+                res_item[path_file] = file_str[start_index:end_index] # 截取搜索到的文本的前后10个字符
+                # print res_item,start_index,end_index
+                res.append(res_item)
+    return res
 
-class add:
-    def GET(self,na):#处理get方法
-        return render.post(na)
-    def POST(self,na):#处理post方法（method）
-        print web.input()['name']#得到表单中的name = 'name'元素的值，也可以这样访问：web.input().name；或先i = web.input()再访问:i.name
-        print web.data()#得到表单提交的所有值，返回形式如：title=title_name&name=add_name&age=24
-        raise web.seeother('/')#转移到首页
+# 将上面搜索到的结果转换为html返回
+def trans2html(search_re):
+    res = ""
+    for item in search_re:
+        for key in item: # 返回给网页的内容要以utf-8编码
+            res = res +  '''
+                <div class="result_item">
+                    <div class="item_fname">''' + key.encode('utf-8') + '''</div>
+                    <div class="item_fcontent">''' + item[key.encode('utf-8')] + '''</div>
+                </div>
+                '''
+    return res
+
+class search:
+    def GET(self):
+        get_data = web.input(search_text={},dir=[])
+        get_str = get_data.search_text.decode('utf-8') # 从网页接收的内容以utf-8解码，在用于搜索
+        print get_str
+        re_html = ""
+        for item in get_data.dir:
+            re_html = re_html + trans2html(search_file(get_str,item.decode('utf-8')))
+        if re_html == "":
+            re_html = '''
+                <div class="result_item">
+                    <div class="item_fname">在所选目录中没有找到包含搜索文本的文件</div>
+                    <div class="item_fcontent">没有搜索到搜索文本</div>
+                </div>'''
+        return re_html
+
+class open_file:
+    """实现打开文件，并返回文件内容"""
+    def GET(self):
+        get_data = web.input(file_name={})
+        if os.path.isfile(get_data.file_name):
+            try:
+                fid = open(get_data.file_name)
+                file_str = fid.read()
+            except:
+                file_str = "文件打开失败！"
+                print file_str
+            finally:
+                fid.close()
+        else:
+            return u"文件不存在！"
+        return file_str
 
 def nf():
     return web.notfound("胡祀鹏 提示：Sorry, the page you were looking for was not found.")
