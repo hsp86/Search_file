050e9793da78ba5794631c1a7180d34eff286d24
hsp86 <husipeng86@live.com>
2016-04-14 20:29:06 +0800

    添加get_exe选项说明

--- get_exe.py
+++ get_exe.py
@@ -3,6 +3,17 @@
 from distutils.core import setup
 import py2exe
 
+# bundle_files有效值为：
+#   3 (默认)不打包。
+#   2 打包，但不打包Python解释器。
+#   1 打包，包括Python解释器。
+options = {"py2exe":{"compressed": 1, #压缩
+                    "optimize": 2, 
+                    "bundle_files": 3 #所有文件打包成一个exe文件 
+                    }
+            }
+
+# setup(console=["search.py"],options=options,zipfile=None) # 64位系统不支持，所以不用
 setup(console=["search.py"])
 
 # 以下移动生成的文件
